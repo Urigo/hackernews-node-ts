44cbad14b6d1cf2966ab133b34ffe9dac5be87f2
urigo <uri.goldshtein@gmail.com>
2024-11-26 13:58:35 +0200

    Step 2.5: Execute first GraphQL query

--- src/schema.ts
+++ src/schema.ts
@@ -1,18 +1,18 @@
-import { createSchema } from 'graphql-yoga'
- 
+import { createSchema } from "graphql-yoga";
+
 const typeDefinitions = /* GraphQL */ `
   type Query {
     hello: String!
   }
-`
- 
+`;
+
 const resolvers = {
   Query: {
-    hello: () => 'Hello World!'
-  }
-}
- 
+    hello: () => "Hello World!",
+  },
+};
+
 export const schema = createSchema({
   resolvers: [resolvers],
-  typeDefs: [typeDefinitions]
-})
+  typeDefs: [typeDefinitions],
+});
