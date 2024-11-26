1901a3a29b7fba24780a914f07af8357a43ae92c
urigo <uri.goldshtein@gmail.com>
2024-11-26 14:55:37 +0200

    Step 7.1: Adding empty context to Yoga Server

--- src/script.ts
+++ src/script.ts
@@ -1,24 +1,24 @@
 // 1
-import { PrismaClient } from '@prisma/client'
- 
+import { PrismaClient } from "@prisma/client";
+
 // 2
-const prisma = new PrismaClient()
- 
+const prisma = new PrismaClient();
+
 // 3
 async function main() {
   const newLink = await prisma.link.create({
     data: {
-      description: 'Fullstack tutorial for GraphQL',
-      url: 'www.howtographql.com'
-    }
-  })
-  const allLinks = await prisma.link.findMany()
-  console.log(allLinks)
+      description: "Fullstack tutorial for GraphQL",
+      url: "www.howtographql.com",
+    },
+  });
+  const allLinks = await prisma.link.findMany();
+  console.log(allLinks);
 }
- 
+
 // 4
 main()
   // 5
   .finally(async () => {
-    await prisma.$disconnect()
-  })
+    await prisma.$disconnect();
+  });
