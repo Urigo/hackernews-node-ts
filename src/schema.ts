0047eb68826e6b655aa8b7edfe4b93f9484a741b
urigo <uri.goldshtein@gmail.com>
2024-11-26 18:36:55 +0200

    Step 9.1: Catch comment on non-existing link error

--- src/schema.ts
+++ src/schema.ts
@@ -1,5 +1,7 @@
 import { createSchema } from 'graphql-yoga'
 import type { Link, Comment } from '@prisma/client'
+import { Prisma } from '@prisma/client'
+import { GraphQLError } from 'graphql'
 import type { GraphQLContext } from './context'
 
 const typeDefinitions = /* GraphQL */ `
@@ -71,6 +73,13 @@ const resolvers = {
           linkId: parseInt(args.linkId),
           body: args.body
         }
+      }).catch((err: unknown) => {
+        if (err instanceof Prisma.PrismaClientKnownRequestError && err.code === 'P2003') {
+          return Promise.reject(
+            new GraphQLError(`Cannot post comment on non-existing link with id '${args.linkId}'.`)
+          )
+        }
+        return Promise.reject(err)
       })
  
       return newComment
