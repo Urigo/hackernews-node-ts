38a25327f0e05966406b91583c01795eb230ebad
urigo <uri.goldshtein@gmail.com>
2024-11-27 22:42:16 +0200

    Step 11.4: Run Codegen initlal generation

---
+++ src/schema/base/resolvers/Mutation/postCommentOnLink.ts
@@ -0,0 +1,6 @@
+import type { MutationResolvers } from "./../../../types.generated";
+export const postCommentOnLink: NonNullable<
+  MutationResolvers["postCommentOnLink"]
+> = async (_parent, _arg, _ctx) => {
+  /* Implement Mutation.postCommentOnLink resolver logic here */
+};
