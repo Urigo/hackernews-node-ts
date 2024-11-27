ecf0186bb60b34c0dbd2b9aac644ea110be65a0f
urigo <uri.goldshtein@gmail.com>
2024-11-27 22:35:31 +0200

    Step 11.2: Create Codegen config

---
+++ codegen.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "@eddeee888/gcg-typescript-resolver-files";
+import type { CodegenConfig } from "@graphql-codegen/cli";
+
+// (1)
+const config: CodegenConfig = {
+  schema: "src/schema/**/schema.graphql", // (2)
+  generates: {
+    "src/schema": defineConfig({
+      // (3)
+      resolverGeneration: "minimal", // (4)
+      typesPluginsConfig: {
+        contextType: "../context#GraphQLContext", // (5)
+      },
+    }),
+  },
+  hooks: { afterAllFileWrite: ["prettier --write"] },
+};
+export default config;
