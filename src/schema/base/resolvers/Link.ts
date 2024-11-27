1c391da3ea77265fb8b2781ea3e8df9df536cb81
urigo <uri.goldshtein@gmail.com>
2024-11-27 22:55:55 +0200

    Step 11.7: Run Codegen after mappers

---
+++ src/schema/base/resolvers/Link.ts
@@ -0,0 +1,16 @@
+import type { LinkResolvers } from "./../../types.generated";
+/*
+ * Note: This object type is generated because "LinkMapper" is declared. This is to ensure runtime safety.
+ *
+ * When a mapper is used, it is possible to hit runtime errors in some scenarios:
+ * - given a field name, the schema type's field type does not match mapper's field type
+ * - or a schema type's field does not exist in the mapper's fields
+ *
+ * If you want to skip this file generation, remove the mapper or update the pattern in the `resolverGeneration.object` config.
+ */
+export const Link: LinkResolvers = {
+  /* Implement Link resolver logic here */
+  comments: async (_parent, _arg, _ctx) => {
+    /* Link.comments resolver is required because Link.comments exists but LinkMapper.comments does not */
+  },
+};
