e1e0acb23c05badb759fe35511909456645ccf1a
urigo <uri.goldshtein@gmail.com>
2024-12-01 19:48:50 +0200

    Step 11.11: Comments must include a link

--- src/schema/base/resolvers/Comment.ts
+++ src/schema/base/resolvers/Comment.ts
@@ -1,10 +1,11 @@
 import type { CommentResolvers } from "./../../types.generated";
+
 export const Comment: CommentResolvers = {
-  link(parent: Comment, _arg: {}, context: GraphQLContext) {
-    return context.prisma.link.findUnique({
+  link(parent, _arg, context) {
+    return context.prisma.link.findUniqueOrThrow({
       where: {
         id: parent.linkId,
       },
     });
-  },
+  }
 };
